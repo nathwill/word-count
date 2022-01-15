4a466e6fecd87247a55c4e946886656300f70653
Nathan Williams <nathan@teamtreehouse.com>
2022-01-15 00:54:53 -0800

    initial commit

---
+++ word-count.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sys
+import math
+import argparse
+
+parser = argparse.ArgumentParser(description = 'Count words at specified character depth')
+parser.add_argument('--filename', type=argparse.FileType('r'), required=True,
+                     default='/usr/share/dict/words')
+parser.add_argument('--depths', metavar='D', type=int, nargs='+', required=True,
+                     help='char depth to traverse word list')
+args = parser.parse_args()
+
+# load word list from file
+def get_words(word_file):
+    return open(word_file).readlines()
+
+# map word count for char depth
+def get_word_dict_at_depth(word_list = [], char_depth = 1):
+    word_dict = {}
+    for word in word_list:
+        if len(word) < char_depth:
+            continue
+
+        s = word[0:char_depth]
+        if word_dict.get(s) == None:
+            word_dict[s] = 1
+        else:
+            word_dict[s] += 1
+
+    return word_dict
+
+# compute avg word count at chart depth
+w = args.filename.read().strip().split("\n")
+
+
+depth_dict = {}
+for d in args.depths:
+    depth_dict[d] = get_word_dict_at_depth(w, d)
+#print(depth_dict)
+
+summary_dict = {}
+for depth, char_count_map in depth_dict.items():
+    counts = char_count_map.values()
+
+    summary_dict[depth] = {}
+    summary_dict[depth]["min"] = min(counts)
+    summary_dict[depth]["avg"] = math.ceil(sum(counts)/len(counts))
+    summary_dict[depth]["max"] = max(counts)
+print(summary_dict)
